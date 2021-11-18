57c092d04961327d4551c3feb8c3a37e5303de2e
AlexeyShak <alexejshakinko@gmail.com>
2021-11-18 23:38:44 +0300

    unit test for the cipher functions

--- tests/atbash.test.js
+++
@@ -1,9 +0,0 @@
-const {atbash} = require('../chipers/atbash');
-
-test('Atbash function should return mirrored only english aphabeth symbols ', () =>{
-    let text = 'AAAAББББzzzzюююю111';
-    expect(atbash(text)).toBe('ZZZZББББaaaaюююю111');
-
-})
-
-
